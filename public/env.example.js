3971230fd55b81e29e33c3eae478a6a8d8d4990b
Manny <manny@His-Mac.local>
2020-08-06 22:06:25 -0400

    fix: 🐛 add default env var values
    Added default values for env.example.js
    
    BREAKING CHANGE: 🧨 n
    
    ✅ Closes: n

--- public/env.example.js
+++ public/env.example.js
@@ -9,28 +9,29 @@ window.API_URL = '';
  * Main GitHub Commit SHA
  * @constant key
  */
-window.GITHUB_COMMIT = '';
+window.GITHUB_COMMIT = 'LOCAL_DEVELOPMENT';
 
 /**
  * Main GitHub Repository URL
  * @constant key
  */
-window.GITHUB_REPO_URL = '';
+window.GITHUB_REPO_URL =
+  'https://github.com/codingwithmanny/github-activity-explorer';
 
 /**
  * Main GitHub Repository URL
  * @constant key
  */
-window.GITHUB_USER = '';
+window.GITHUB_USER = 'codingwithmanny';
 
 /**
  * Main Netlify Deploy ID
  * @constant key
  */
-window.DEPLOY_ID = '';
+window.DEPLOY_ID = 'LOCAL_DEPLOYMENT';
 
 /**
  * Main Netlify Deploy URL
  * @constant key
  */
-window.DEPLOY_URL = '';
+window.DEPLOY_URL = '/';
